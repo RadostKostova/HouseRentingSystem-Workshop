63d3a3956a4f3edb3e0067494e5915c4a3e2f069
Radost Kostova <radostt_kostova@abv.bg>
2024-03-01 21:40:49 +0200

    Agent service implemented

--- HouseRentingSystem.Core/Services/AgentService.cs
+++ HouseRentingSystem.Core/Services/AgentService.cs
@@ -14,9 +14,15 @@ namespace HouseRentingSystem.Core.Services
             repository = _repository;
         }
 
-        public Task CreateAsync(string userId, string phoneNumber)
+        public async Task CreateAsync(string userId, string phoneNumber)
         {
-            throw new NotImplementedException();
+            await repository.AddAsync(new Agent()
+            {
+                UserId = userId,
+                PhoneNumber = phoneNumber
+            });
+
+            await repository.SaveChangesAsync();
         }
 
         public async Task<bool> ExistsByIdAsync(string userId)
@@ -26,14 +32,16 @@ namespace HouseRentingSystem.Core.Services
                 .AnyAsync(a => a.UserId == userId);
         }
 
-        public Task<bool> UserHasRentsAsync(string userId)
+        public async Task<bool> UserHasRentsAsync(string userId)
         {
-            throw new NotImplementedException();
+            return await repository.AllReadOnly<House>()
+                .AnyAsync(h => h.RenterId == userId);
         }
 
-        public Task<bool> UserWithPhoneNumberExistAsync(string phoneNumber)
+        public async Task<bool> UserWithPhoneNumberExistAsync(string phoneNumber)
         {
-            throw new NotImplementedException();
+            return await repository.AllReadOnly<Agent>()
+                .AnyAsync(a => a.PhoneNumber == phoneNumber);
         }
     }
 }
--- HouseRentingSystem/Extensions/ClaimsPrincipalExtensions.cs
+++ HouseRentingSystem/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,10 +1,10 @@
 ﻿namespace System.Security.Claims;
+
+public static class ClaimsPrincipalExtensions
 {
-    public static class ClaimsPrincipalExtensions
+    public static string Id(this ClaimsPrincipal user)
     {
-        public static string Id(this ClaimsPrincipal user)
-        {
-            return user.FindFirstValue(ClaimTypes.NameIdentifier);
-        }
+        return user.FindFirstValue(ClaimTypes.NameIdentifier);
     }
 }
+
